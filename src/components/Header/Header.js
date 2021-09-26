3b35ea3ac4507d9620deb1a1822c0020eac8348e
TahminaChowdhury <tahmina10st@gmail.com>
2021-09-26 21:17:35 +0600

    commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,11 +10,11 @@ const Header = () => {
                     <a href="/Sculpture">Sculpture</a>
                     <a href="/Drawing">Drawing</a>
                     <a href="/pricing">Pricing</a>
-                    <span className="input"><input type="text" placeholder="Search here..." /><button className="btn btn-outline-light text-white">Click</button></span>
+                    <span><input className="input" type="text" placeholder="Search here..." /><button className="click-button">Click</button></span>
             </nav>
            <div className="w-50 text-center text-light  py-5 mb-3 mt-5 mx-auto">
                <h1 className="heading text-uppercase">Art is an irreplaceable way of understanding and expressing the world</h1>
-               <h3>Total Budget: 50 Million</h3>
+               <h3 className="py-3">Total Budget: 50 Million</h3>
            </div>
         </div>
     );
--- src/components/Total/Total.js
+++ src/components/Total/Total.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Total.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faUser} from '@fortawesome/free-solid-svg-icons'
 
@@ -9,14 +10,22 @@ const Total = (props) => {
     let artistName ='';
     for(const artist of artists ){
         total = total + artist.salary;
-        artistName= artistName+ ' ' +artist.name;
     }
     
     return (
         <div className="text-white text-center pt-2">
             <h3 className="fw-bold"><FontAwesomeIcon icon={faUser} /> Artists Added: {props.artist.length}</h3>
-            <h4 className="fw-bold">Total Cost: $ {total}</h4>
-            
+            <h4 className="fw-bold pt-2">Total Cost: $ {total}</h4>
+            <ul>
+                {artists.map(artist => {
+                    return (
+                        <div className="bg-light rounded-3 mt-4 d-flex align-center" id="name-div">
+                            <img className="w-25" src={artist.img} alt="" />
+                            <h6 className=" text-dark py-2 ms-4">{artist.name}</h6>
+                        </div>
+                    )
+                })}
+            </ul>
             <button className="btn btn-outline-primary w-25">Buy</button>
         </div>
     );
